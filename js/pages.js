d5e6a4f4d123f00ae0b875e81913ace9758f8362
Pedro-Spindola <spindolapedrohenrique99@gmail.com>
2023-05-19 02:18:10 -0300

    Updates

--- js/pages.js
+++ js/pages.js
@@ -1,11 +1,13 @@
-const btnClick1A = document.querySelector(".btn-p1-A");
+const btnClick = document.querySelector(".btn-iniciar")
 const btnClick2A = document.querySelector(".btn-p2-A");
 const btnClick2V = document.querySelector(".btn-p2-V");
+const btnClick3A = document.querySelector(".btn-p3-A");
+const btnClick3V = document.querySelector(".btn-p3-V");
 const page1 = document.querySelectorAll(".form-box-p1");
 const page2 = document.querySelectorAll(".form-box-p2");
+const page3 = document.querySelectorAll(".form-box-p3");
 
-
-btnClick1A.addEventListener("click", function() {
+btnClick.addEventListener("click", function() {
     this.classList.toggle("disable");
 
     page1.forEach(icon => {
@@ -17,12 +19,12 @@ btnClick1A.addEventListener("click", function() {
         icon.classList.remove("disable");
         icon.classList.toggle("active");
     });
-    btnClick1A.classList.toggle("active");
+    btnClick.classList.toggle("active");
 });
 
 btnClick2A.addEventListener("click", function() {
     this.classList.toggle("disable");
-    
+
     page2.forEach(icon => {
         icon.classList.remove("active");
         icon.classList.toggle("disable");  
@@ -32,6 +34,7 @@ btnClick2A.addEventListener("click", function() {
         icon.classList.remove("disable");
         icon.classList.toggle("active");
     });
+    btnClick2A.classList.toggle("active");
 });
 
 btnClick2V.addEventListener("click", function() {
@@ -46,5 +49,36 @@ btnClick2V.addEventListener("click", function() {
         icon.classList.remove("disable");
         icon.classList.toggle("active");
     });
+    btnClick2V.classList.toggle("active");
+});
+
+btnClick3A.addEventListener("click", function() {
+    this.classList.toggle("disable");
+
+    page3.forEach(icon => {
+        icon.classList.remove("active");
+        icon.classList.toggle("disable");  
+    });
+
+    page4.forEach(icon => {
+        icon.classList.remove("disable");
+        icon.classList.toggle("active");
+    });
+    btnClick3A.classList.toggle("active");
+});
+
+btnClick3V.addEventListener("click", function() {
+    this.classList.toggle("disable");
+    
+    page3.forEach(icon => {
+        icon.classList.remove("active");
+        icon.classList.toggle("disable");  
+    });
+
+    page2.forEach(icon => {
+        icon.classList.remove("disable");
+        icon.classList.toggle("active");
+    });
+    btnClick3V.classList.toggle("active");
 });
 
